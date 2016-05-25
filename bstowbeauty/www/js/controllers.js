42e62fbe23e6a436d03997f6f5db04043d90d488
KaDy123 <kdodson.bcc@gmail.com>
2016-05-25 10:52:33 -0400

    initial upload

---
+++ bstowbeauty/www/js/controllers.js
@@ -0,0 +1,17 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('FacebookCtrl', function($scope) {})
+
+.controller('FindCtrl', function($scope) {})
+
+.controller('AccountCtrl', function($scope) {})
+
+.controller('ServicesCtrl', function($scope) {})
+
+.controller('AppointmentsCtrl', function($scope) {})
+
+.controller('SpecialsCtrl', function($scope) {})
+
+.controller('PricesCtrl', function($scope) {});
